7deed67767908044d6eb75f5376eab935bcdecd4
Alex Draper <mradraper@gmail.com>
2022-12-07 12:44:55 -0700

    Wrote a function that repeats a string a given number of times, completing the second Odin Project exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if(num < 0) {
+        return "ERROR";
+    }
+
+    if(num === 0) {
+        return "";
+    }
+
+    if(num === 1) {
+        return string;
+    }
+
+    else{
+        return string + repeatString(string, num - 1);
+    }
 
 };
 
