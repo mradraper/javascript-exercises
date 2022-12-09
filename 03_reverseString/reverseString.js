8e5d0ed32f25a458615b1d3d5b2d7d7527f7879e
Alex Draper <mradraper@gmail.com>
2022-12-09 01:33:33 -0700

    Improved the comments on the reverseString function and completed the removeFromArray Odin Project exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,13 @@
 const reverseString = function(string) {
 
     var splitString = string.split("");
+//turns the characters in the string into an array
 
     var reverseArray = splitString.reverse();
+//reverses the order of the array of characters
 
     var joinArray = reverseArray.join("");
+//joins together the array of characters into a string
 
     return joinArray;
 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray, ...toBeRemoved) {
+    return myArray.filter(checkItem => !toBeRemoved.includes(checkItem))
+    //return the items in the array that are not found in the toBeRemoved list
 };
 
 // Do not edit below this line
