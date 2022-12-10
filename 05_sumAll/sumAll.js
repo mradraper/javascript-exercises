12291877bf19532c580b9ef53f2320039a0f8c11
Alex Draper <mradraper@gmail.com>
2022-12-10 01:35:29 -0700

    Wrote a function that passes all the tests for the sumAll exercise of Odin Project's Foundations course

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
 
+    let sumNums = 0;
+
+    if (firstNum < 0 || secondNum < 0){
+        return "ERROR";
+    } 
+    
+    else if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum) ){
+        return "ERROR";
+    }
+
+    else {
+
+        for (i = Math.min(firstNum, secondNum); i < Math.max(firstNum, secondNum) + 1; i++){
+
+        sumNums += i;
+
+        }
+
+        return sumNums;
+    }
 };
 
 // Do not edit below this line
