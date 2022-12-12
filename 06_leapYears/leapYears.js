974c0411b96decb9b21f6d60ce610da6492b73f0
Alex Draper <mradraper@gmail.com>
2022-12-12 00:11:48 -0700

    Completed the leapYears Odin Project exercise and passed all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,26 @@
-const leapYears = function() {
+const leapYears = function(testYear) {
+    if (testYear % 4 === 0 && testYear % 100 === 0 && testYear % 400 === 0) {
+        return true;
+    }
+    // Is the year divisible by 4, divisible by 100, and divisible by 400? Then it is a leap year.
+
+    else if (testYear % 4 === 0 && testYear % 100 === 0 && testYear % 400 != 0) {
+        return false;
+    }
+    // Is the year divisible by 4, divisible by 100, but not divisible by 400? Then it is not a leap year.
+
+    else if (testYear % 4 === 0 && testYear % 100 != 0 ) {
+        return true;
+    }
+    // Is the year divisible by 4 and not by 100? Then it is a leap year.
+
+    else {
+        return false;
+    }
+    // Otherwise (if it is not divisible by 4), it is not a leap year.
+    
+
+
 
 };
 
