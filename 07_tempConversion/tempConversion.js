c7f493306d812efae04355e7c1afa68b5b71e3b4
Alex Draper <mradraper@gmail.com>
2022-12-12 00:39:43 -0700

    Completed the tempConversion exercises for the Odin Project.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitEntry) {
+  celsiusResultLong = (farenheitEntry - 32) * (5/9);
+  celsiusResult = Math.round(celsiusResultLong * 10) / 10;
+  return celsiusResult;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusEntry) {
+  farenheitResultLong = (celsiusEntry * (9/5)) + 32;
+  farenheitResult = Math.round(farenheitResultLong * 10) / 10;
+  return farenheitResult;
 };
 
 // Do not edit below this line
