406b8c785baa9e3a39b50ef8831d64a570b0c103
Alex Draper <mradraper@gmail.com>
2022-12-12 00:44:36 -0700

    Added comments to the tempConversion functions.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,20 @@
 const convertToCelsius = function(farenheitEntry) {
   celsiusResultLong = (farenheitEntry - 32) * (5/9);
+  // Converts the farenheit entry into celsius.
+
   celsiusResult = Math.round(celsiusResultLong * 10) / 10;
+  // Round the celsius number to one decimal point.
+
   return celsiusResult;
 };
 
 const convertToFahrenheit = function(celsiusEntry) {
   farenheitResultLong = (celsiusEntry * (9/5)) + 32;
+  // Converts the celsius entry into farenheit.
+
   farenheitResult = Math.round(farenheitResultLong * 10) / 10;
+  // Round the celsisu number to one decimal point.
+  
   return farenheitResult;
 };
 
